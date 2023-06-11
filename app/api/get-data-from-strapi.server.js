d146beb7ec5db8a2fc298182e54f145e402d5cdd
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-06-11 16:35:19 -0400

    Baseline 1.20
    Added strapi API token

--- app/api/get-data-from-strapi.server.js
+++ app/api/get-data-from-strapi.server.js
@@ -3,7 +3,11 @@ export async function getDataFromStrapi(path, query) {
 console.log(url);
 console.log('po');
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer 8d6bc6426e15cd3f2258c808296c55de0a56f144520131471d3821ba329a6a0aecfeb1c25280c7c51812c5f9a11cfbbefe804434e22f186266aee12f990d45467e8f9f139f700760e0bc0d9512eaa30d3c7a7f26296d9cf9ef9db9b571d6f307c3a05a2080c13ac74b44cbab9a02d5169ee68ccfd3547a711a5953c5c2f81c8e`,
+      },
+    });
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
