355b5b4b1a52aef82ca34e71b45d0e5dcce04d89
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-07-15 22:13:31 -0400

    Baseline 2.4
    Updated API to work with new strapi DB

--- .netlify/functions-internal/server.js
+++ .netlify/functions-internal/server.js
@@ -155,7 +155,7 @@ init_react();
 async function getDataFromStrapi(path, query) {
   try {
     let baseUrl3, apiToken;
-    baseUrl3 = "http://127.0.0.1:1337", apiToken = "4e789b61865506da6c5b0d0378cc6d826d7c205da6128777941b4af626ed58b95de55f76f0f1adc48f2071561a72554ce0b501a74b4f91a81d6367efcc37cf504d20a0e95db3fa0a70ebc7be62079855f34628228c703a5de19563391d6b9725a42a4f6842e65cb131ae166824fafbd8ab371414ec4dad174d29ff600e1d4ca7", console.log("This is a local build yo");
+    baseUrl3 = "http://127.0.0.1:1337", apiToken = "ef17f73a62fa3256dcabba3f5dc9215c2d6dab18986b49c841286aa1186e183a45c8743af6f8f203acafcb290731d8d8aa70d753f22c40b727faf1cafa5be35c94c78884919175ed4e234b82057db9392d471d8cd72fbcdaa6266e8fa2a6dd3c7727cead41e656f063bd86dd25cab091b0db486924b94c3f455c32651fb07f14", console.log("This is a local build yo");
     let url = `${baseUrl3}/api/${path}?${query}`, response = await fetch(url, {
       headers: {
         Authorization: `Bearer ${apiToken}`
